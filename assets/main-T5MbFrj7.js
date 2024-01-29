ae3eb8bb3120dad0169ce2132ad8d425c96d09a8
daw2-viri23 <113451228+daw2-viri23@users.noreply.github.com>
2024-01-29 15:22:33 +0000

    Updates

--- assets/main-T5MbFrj7.js
+++ assets/main-T5MbFrj7.js
@@ -92,10 +92,88 @@ const __vitePreload = function preload(baseModule, deps, importerUrl) {
   });
 };
 const header = {
-  template: `header`
+  template: (
+    // html
+    `
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+      <a class="navbar-brand" href="#"
+        ><img
+          src="images/logo.svg"
+          alt=""
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+  
+        Vanilla Games</a
+      >
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">TOP5 Proyectos</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" aria-current="page" href="#">A cerca de</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav ms-auto me-2 mb-2 mb-lg-0">
+          <li class="nav-item">
+            <button class="ms-2 btn btn-success">
+              Iniciar sesión
+              <i class="bi bi-box-arrow-in-right"></i>
+            </button>
+          </li>
+          <li class="nav-item">
+            <button class="ms-2 btn btn-outline-light">
+              Regístrate
+              <i class="bi bi-box-arrow-in-right"></i>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  
+    `
+  )
 };
 const footer = {
-  template: `footer`
+  template: (
+    // html
+    `
+  <nav class="navbar bg-secondary fixed-bottom small">
+    <div class="container">
+      <a class="navbar-brand" href="http://www.fpllefia.com">
+        <img
+          src="images/logo.svg"
+          alt="fpllefia"
+          width="30"
+          height="24"
+          class="d-inline-block align-text-top"
+        />
+        FPLlefià
+      </a>
+      <span class="navbar-text">@Texto de header</span>
+      <a href="#" class="nav-link">Vínculo header</a>
+    </div>
+  </nav>
+    `
+  )
 };
 var top = "top";
 var bottom = "bottom";
@@ -5144,7 +5222,7 @@ class Toast extends BaseComponent {
 enableDismissTrigger(Toast);
 defineJQueryPlugin(Toast);
 async function cargarVista() {
-  const componente = await __vitePreload(() => import("./homeVista-VvFcafcq.js"), true ? __vite__mapDeps([]) : void 0, import.meta.url);
+  const componente = await __vitePreload(() => import("./registroVista-KheGX-E2.js"), true ? __vite__mapDeps([]) : void 0, import.meta.url);
   const vista = componente.default;
   document.querySelector("main").innerHTML = vista.template;
 }
---
+++ assets/registroVista-KheGX-E2.js
@@ -0,0 +1,53 @@
+const registroVista = {
+  template: (
+    // html
+    `
+    
+
+    <div class="container">
+    <h1 class="mt-5 text-center">Registro</h1>
+    <div class="m-5 mx-auto" style="max-width: 400px">
+        <form action="" class="form border shadow-sm p-3">
+
+            <!-- Nombre -->
+            <label for="nombre" class="form-label">Nombre:</label>
+            <input required id="nombre" type="text" class="form-control" />
+            
+            <!-- Apellidos -->
+            <label for="apellidos" class="form-label">Apellidos:</label>
+            <input id="apellidos" type="text" class="form-control" />
+            
+            <!-- Email -->
+            <label for="email" class="form-label">Email:</label>
+            <input required id="email" type="email" class="form-control" />
+            
+            <!-- Contraseña -->
+            <label for="pass" class="form-label mt-3">Contraseña:</label>
+            <input required id="pass" type="password" min="6" class="form-control" />
+            
+            <!-- Botón enviar -->
+            <input type="submit" class="btn btn-primary w-100 mt-3" value="Enviar" />
+          </form>
+    </div>
+</div>
+                
+
+
+
+    `
+  ),
+  script: () => {
+    console.log("vista registro cargada");
+    const formulario = document.querySelector("#formRegistro");
+    formulario.addEventListener("submit", (event) => {
+      event.preventDefault();
+      event.stopPropagation();
+      if (!formulario.checkValidity()) {
+        formulario.classList.add("was-validated");
+      }
+    });
+  }
+};
+export {
+  registroVista as default
+};
