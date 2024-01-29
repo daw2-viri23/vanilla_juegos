b526d148d24803bd61d0d48b3f64b77adeb8c4a6
daw2-viri23 <113451228+daw2-viri23@users.noreply.github.com>
2024-01-29 15:16:22 +0000

    commit de los commits de las vistas header y el menu de edicion de perfil

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import { header } from './componentes/header'
-import { footer } from './componentes/footer'
-// Import all of Bootstrap's JS
-import 'bootstrap'
-
 // Import our custom CSS
 import './scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
 
-// Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/registroVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-}
-cargarVista()
+// Importamos componentes header y footer
+import { header } from './componentes/header.js'
+import { footer } from './componentes/footer'
 
-//Inyectamos el componente header
+// Importamos la Función para detectar eventos al cargar las vistas
+import { enrutador } from './componentes/enrutador'
+
 document.querySelector('header').innerHTML = header.template
-
-//Inyectamos el componente footer
+header.script()
 document.querySelector('footer').innerHTML = footer.template
 
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
