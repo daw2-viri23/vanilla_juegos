40ba76872ced0ed2aac9b7380245ac54ec66c4d2
daw2-viri23 <113451228+daw2-viri23@users.noreply.github.com>
2024-03-08 13:44:37 +0000

    pagina 404 actualizada

--- src/vistas/404.js
+++ src/vistas/404.js
@@ -1,6 +1,14 @@
 export default{
     template: `
-        <h1>XD</h1>
+    <div class="container">
+    <div class="row">
+      <div class="col-md-12 error-container">
+        <h1 class="error-heading">Error 404</h1>
+        <p class="error-message">Lo sentimos, la página que estás buscando no pudo ser encontrada.</p>
+        <a href="/" class="btn btn-primary">Volver a la página de inicio</a>
+      </div>
+    </div>
+  </div>
     `,
     script: ()=>{
         console.log('XD')
